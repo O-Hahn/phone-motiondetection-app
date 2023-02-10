960f2ca662e494266ed4e2cdaa9c03393102edbe
Olaf Hahn <olaf_hahn@at.ibm.com>
2023-02-10 17:57:00 +0100

    next.config test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: 'standalone',
+  // Will be available on both server and client
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_SEND_ORIENTATION: process.env.NEXT_PUBLIC_SEND_ORIENTATION, 
+
+    NEXT_PUBLIC_CLOUDANT_HOST_URL: process.env.NEXT_PUBLIC_CLOUDANT_HOST_URL,
+    NEXT_PUBLIC_CLOUDANT_USER: process.env.NEXT_PUBLIC_CLOUDANT_USER,
+    NEXT_PUBLIC_CLOUDANT_PASSWORD: process.env.NEXT_PUBLIC_CLOUDANT_PASSWORD
+  },
+  output: 'standalone'
 }
