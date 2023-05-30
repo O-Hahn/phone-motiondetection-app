00616f53a998f5528f7c857218352912c6fc50c9
Olaf Hahn <olaf_hahn@at.ibm.com>
2023-05-30 14:01:14 +0000

    train test

--- pages/api/TrainCloudant.js
+++ pages/api/TrainCloudant.js
@@ -15,6 +15,9 @@ export default async function handler(req, res) {
 
 }
 
+// https://apikey-v2-ihd5hebqdeyt15h5u4nfyeb1unajlsbkj2jvhvinxs6:a731325dbb0c1e2200bd3e8121cc035d@300c5094-e63a-44c5-9df1-faf9c80bad9c-bluemix.cloudantnosqldb.appdomain.cloud/
+// https://apikey-v2-21stpso9b22c2x51efm5hck79945towgbfhe5738l0al:4a450f3d44d60350ffd999041bef9f4e@312c7150-eb85-42ac-83f5-225a79aeb266-bluemix.cloudantnosqldb.appdomain.cloud/new_train
+
 async function sendTrainCloudant(req, res) {
     try {
         console.log("TrainCloudant.sendTrainCloudant()");
@@ -23,6 +26,7 @@ async function sendTrainCloudant(req, res) {
         let url = data.url;
         console.log("train CloudantDB req url = " + url);
         console.log(data);
+        console.log(JSON.stringify(data.dataObj));
 
         let resp = null;
         await fetch(url, {
